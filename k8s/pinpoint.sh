b3b70c9b758e1fdbe7b9df2c76653e35f661bdb3
FullMooney <hs3235@naver.com>
2024-05-28 17:21:57 +0900

    helm 변환중

---
+++ k8s/pinpoint.sh
@@ -0,0 +1,21 @@
+kubectl create ns pinpoint
+
+kubectl create cm pinpoint-config -n pinpoint  --from-file=../.env
+
+kubectl create -f chart/zookeeper/zk-statefulset.yaml -n pinpoint
+
+kubectl create -f chart/zookeeper/zk-service.yaml -n pinpoint
+
+kubectl create -f chart/mariadb/mariadb-statefulset.yaml -n pinpoint
+
+kubectl create -f chart/mariadb/mariadb-service.yaml -n pinpoint
+
+kubectl create -f chart/hbase/hbase-statefulset.yaml -n pinpoint
+
+kubectl create -f chart/hbase/hbase-service.yaml -n pinpoint
+
+kubectl create -f chart/collector/collector-deployment.yaml -n pinpoint
+
+kubectl create -f chart/collector/collector-service.yaml -n pinpoint
+
+
